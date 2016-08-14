83d3c870b95539dfb5f4149960de29b4d75c9641
汤宝铮 <baozhengta@163.com>
2016-08-14 22:41:53 +0800

    mine new fun

--- BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
+++ BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
@@ -21,6 +21,10 @@
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 }
+-(void)viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
