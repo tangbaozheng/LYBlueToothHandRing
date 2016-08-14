03a8c30f46c8d599022b74192de0898f3908037a
汤宝铮 <baozhengta@163.com>
2016-08-14 22:49:51 +0800

    xo

--- BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
+++ BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
@@ -18,4 +18,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+-(void)ahha {
+    
+}
 @end
