86c3e754380763079f955cfb96acf4da5a79ac42
汤宝铮 <baozhengta@163.com>
2016-08-14 22:40:27 +0800

    new fun

--- BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
+++ BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
@@ -18,7 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
