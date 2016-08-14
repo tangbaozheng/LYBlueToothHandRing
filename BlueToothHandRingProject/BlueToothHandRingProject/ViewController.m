203b8f9ac1702d6e6b5c9f349bcd1723b072564e
汤宝铮 <baozhengta@163.com>
2016-08-14 22:55:37 +0800

    use mine a

--- BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
+++ BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
@@ -18,4 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
 @end
