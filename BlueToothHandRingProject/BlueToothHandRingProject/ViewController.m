2e5eb50fe8639d2c84980a16afe5aed5e6add5a9
汤宝铮 <baozhengta@163.com>
2016-08-14 22:54:30 +0800

    all delete

--- BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
+++ BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
@@ -18,9 +18,4 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
--(void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    //////
-}
 @end
