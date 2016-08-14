6feb5835e1e0eee2185f96342b5fe7d3b9c288c5
汤宝铮 <baozhengta@163.com>
2016-08-14 22:42:16 +0800

    mine delete

--- BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
+++ BlueToothHandRingProject/BlueToothHandRingProject/ViewController.m
@@ -21,9 +21,4 @@
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
